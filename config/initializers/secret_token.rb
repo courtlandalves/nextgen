6e7b2952183c714d6240cff8cf58734d1953a0ea
courtlandalves <courtlandalves@gmail.com>
2013-02-12 21:27:07 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nextgen::Application.config.secret_token = '014d0b761b8eaffee94b3be067265ed96739da0433907baf51cb8398e26ec981f76dc0c8586cc95905abcb33d859a29d5382797eae76fe6335f68752ee49b91b'
