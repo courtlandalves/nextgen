5877de8365660cb0cd5808192c860579c9f73c93
courtlandalves <courtlandalves@gmail.com>
2013-08-07 16:22:47 -0700

    Admin Firms page has images

--- app/admin/firms.rb
+++ app/admin/firms.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register Firm do
 
-  index do
-    column :image_url
-    column :url
-    default_actions
+  index :as => :grid do |firm|
+    div_for(firm, {:style => "display:table; height: 50px; text-align: center; width:100%"}) do |firm|
+      link_to(image_tag(firm.image_url, {:style => "max-height:50px; max-width: 100%; border: 1px solid; "}), admin_firm_path(firm), {:style => "display:table-cell; vertical-align:middle; text-aling:center;"})
+    end
   end
 
 end
