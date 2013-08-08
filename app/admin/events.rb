72a734879c20cf56da1c79396c069fe285587c41
courtlandalves <courtlandalves@gmail.com>
2013-08-08 16:03:04 -0700

    Getting ImageMagick working and creating thumbs of events and good admin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,10 +1,25 @@
 ActiveAdmin.register Event do
+  config.sort_order = 'date_desc'
+  filter :title
+  filter :date
 
   index do
+    column :image do |event|
+      link_to(image_tag(event.image_url(:thumb).to_s), admin_event_path(event))
+    end
     column :title
     column :date
-    column :image
     default_actions
+  end
+
+  show :title => :title do |event|
+    attributes_table do
+      row :title
+      row :date
+      row :image do
+        image_tag(event.image_url)
+      end
+     end
   end
 
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -29,12 +29,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   #Once we get RMagick to work:
-  #version :thumb do
-  #  process :resize_to_limit => [70, 53]
-  #end
+  version :thumb do
+    process :resize_to_limit => [70, 53]
+  end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_limit => [1000, 800]
   #
 
   # Create different versions of your uploaded files:
