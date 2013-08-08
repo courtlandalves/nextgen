48698e6eecbff891c86a7a70fdb1e073e0d6db57
courtlandalves <courtlandalves@gmail.com>
2013-08-08 16:18:09 -0700

    Fixing events admin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -8,18 +8,31 @@ ActiveAdmin.register Event do
       link_to(image_tag(event.image_url(:thumb).to_s), admin_event_path(event))
     end
     column :title
-    column :date
+    column :date do |event|
+      event.date.strftime('%B, %Y')
+    end
     default_actions
   end
 
   show :title => :title do |event|
     attributes_table do
       row :title
-      row :date
+      row :date do |event|
+        event.date.strftime('%B, %Y')
+      end
       row :image do
         image_tag(event.image_url)
       end
      end
+  end
+ 
+  form do |f|
+    f.inputs "Event Info" do
+      f.input :title
+      f.input :date
+      f.input :image_url, :as => :file
+    end
+    f.buttons
   end
 
 end
