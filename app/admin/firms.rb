05658c05cadcc8ee564f35eca00e05621d033fee
courtlandalves <courtlandalves@gmail.com>
2013-08-07 17:23:14 -0700

    Moving css files and polishing up firms admin

--- app/admin/firms.rb
+++ app/admin/firms.rb
@@ -1,8 +1,20 @@
 ActiveAdmin.register Firm do
+  filter :url
+  config.sort_order = "url_asc"
+  config.paginate = false
 
   index :as => :grid do |firm|
-    div_for(firm, {:style => "display:table; height: 50px; text-align: center; width:100%"}) do |firm|
-      link_to(image_tag(firm.image_url, {:style => "max-height:50px; max-width: 100%; border: 1px solid; "}), admin_firm_path(firm), {:style => "display:table-cell; vertical-align:middle; text-aling:center;"})
+    div_for(firm, {:style => "display:table; height: 50px;  width: 100%; text-align: center;"}) do |firm|
+      link_to(image_tag(firm.image_url, {:style => "max-height:50px; max-width: 100%; border: 1px solid;"}), admin_firm_path(firm), {:style => "display:table-cell; vertical-align:middle;"})
+    end
+  end
+
+  show :title => :url do |firm|
+    attributes_table do
+      row :url
+      row :image do
+        image_tag(firm.image_url, {:style => "border: 1px solid;"})
+      end
     end
   end
 
