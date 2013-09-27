97b435693b5fe95f8aa42dd93275f27bed227c85
courtlandalves <courtlandalves@gmail.com>
2013-09-27 11:33:02 -0700

    Showing all events in nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def load_events
-    @events = Event.order('date DESC').limit(4)
+    @events = Event.order('date DESC')
   end
 end
